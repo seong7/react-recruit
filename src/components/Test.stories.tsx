1a7f0c95f317bffbf6621bbe6038678e19cb918f
seong7 <seongjinkim1123@gmail.com>
2022-04-01 19:29:10 +0900

    feat(*): Add dependency - Emotion

--- src/components/Test.stories.tsx
+++ src/components/Test.stories.tsx
@@ -10,5 +10,5 @@ const Template: ComponentStory<typeof Test> = (args) => <Test {...args} />;
 
 export const Basic = Template.bind({});
 Basic.args = {
-  text: '',
+  text: 'default text',
 };
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/css';
 
 export interface TestProps {
   text: string;
 }
 
 export const Test = ({ text }: TestProps) => {
-  return <>{text}</>;
+  return (
+    <div
+      className={css`
+        text-align: end;
+      `}
+    >
+      <StyledP>{text}</StyledP>
+    </div>
+  );
 };
+
+const StyledP = styled.p`
+  color: cadetblue;
+`;
