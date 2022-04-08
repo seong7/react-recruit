f9df907af42d2a32e9ae57cd5a8a63f391d463b7
seong7 <seongjinkim1123@gmail.com>
2022-04-08 13:54:12 +0900

    refactor(assets/svg): change how the svgs exported

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import MenuIcon from './svg/menu.svg';
+import LoaderIcon from './svg/loader.svg';
+import ArrowLeftIcon from './svg/arrow_left.svg';
+
+export { MenuIcon, LoaderIcon, ArrowLeftIcon };
